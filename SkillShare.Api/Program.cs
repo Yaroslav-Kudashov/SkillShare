b9d6c9b71af5eebeeb8a801a332f117890bcce7a
kudashov Yaroslav <kudasovaroslav27@gmail.com>
2026-02-24 17:26:38 +0300

    Добавил RebbitMq

--- SkillShare.Api/Program.cs
+++ SkillShare.Api/Program.cs
@@ -2,9 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using Serilog;
 using SkillShare.Api.Middlewares;
 using SkillShare.Application.DependencyInjection;
+using SkillShare.Consumer.DependencyInjection;
 using SkillShare.DAL;
 using SkillShare.DAL.DependencyInjection;
 using SkillShare.Domain.Settings;
+using SkillShare.Producer.DependencyInjection;
 namespace SkillShare.Api;
 
 public class Program
@@ -13,18 +15,18 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.Configure<RabbitMqSettings>(builder.Configuration.GetSection(nameof(RabbitMqSettings)));
         builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection(JwtSettings.DefaultSection));
 
         builder.Services.AddDataAccessLayer(builder.Configuration);
-
         builder.Services.AddControllers();
+        builder.Services.AddProducer();
+        builder.Services.AddConsumer();
 
         builder.Services.AddAuthenticationAndAuthorization(builder);
-
         builder.Services.AddSwagger();
 
         builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
-
         
         builder.Services.AddApplication();
 
---
+++ SkillShare.Domain/Settings/RabbitMqSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillShare.Domain.Settings;
+
+public class RabbitMqSettings
+{
+    public string HostName { get; set; }
+
+    public int Port { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Password { get; set; } 
+
+    public string QueueName { get; set; } 
+
+    public string RoutingKey { get; set; }
+
+    public string ExchangeName { get; set; } 
+}
